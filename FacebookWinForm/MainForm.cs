2b7eef05f980cb959ba9866772bab230e517e6f5
Diamant <ndiamant@paypal.com>
2018-07-22 21:19:37 +0300

    initial

---
+++ FacebookWinForm/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace FacebookWinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //InitializeComponent();
+            FacebookService.s_CollectionLimit = 200;
+            FacebookService.s_FbApiVersion = 2.8f;
+        }
+    }
+}
---
+++ FacebookWinForm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using FacebookWrapper;
+
+namespace FacebookWinForm
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Clipboard.SetText("designpatterns");
+            FacebookService.s_UseForamttedToStrings = true;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+    }
+}
